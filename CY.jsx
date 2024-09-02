c6d70654273946f7ab64db74d7a7f615ea0cf31a
Yaswanth G <727723eucs282@skcet.ac.in>
2024-09-02 12:29:14 +0530

    DAY 2

--- CY.jsx
+++ CY.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
-const D2cy=()=>{
-    const image="/smile_image.jpeg";
-    const dstyle={
-        backgroundColor:"lightblue",width:'300px',height:'350px',padding:'100px',border:'1px solid blue',
-    };
-    return(
-        <center><div style={dstyle}>
-        <h1><b><center>Smile Component</center></b></h1>
-        <p style={{height:"100px"}}><center>It is a functional component</center></p>
-        <center><img src={image} alt="Not found" height="100px" weght="100px"></img></center>
-        </div></center>
-    )
+import React from 'react';
+const instyle={
+      backgroundColor:'lightblue',
+      border:'3px solid black',
+      margin:'200px',
+    height:'300px',
+    width:'400px'
+    
+ };
+class Weather extends React.Component{
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            location:'Newyork',
+            Temperature:'20°C',
+            Condition:'Sunny'
+        }
+
+    }
+    Click = (event) => {
+        event.preventDefault(); 
+        
+        const Loc = document.getElementById('loc');
+        const Locval = Loc.value;
+        
+        this.setState((pre)=>({location:pre.location==='Newyork'?Locval :'Newyork'}))
+                
+    }
+    
+   
+    render()
+    {
+        return(
+            <center><form onSubmit={this.Click} style={instyle}>
+            <h1>WEATHER APPLICATION</h1>
+            <input type="text" placeholder="Enter new Location" id="loc" required/>
+            <button type='submit'>Update Location</button>
+            <h4>Current Weather:{this.state.location}</h4>
+            <h4>Temperture:{this.state.Temperature}</h4>
+            <h4>Condition:{this.state.Condition}</h4>
+            </form></center>
+        )
+    }
 }
-export default D2cy;
+export default Weather;
--- PAH.jsx
+++ PAH.jsx
@@ -1,53 +1,37 @@
 import React from 'react';
-const Day2pah1=()=>{
-    const instyle={
-        margintop:'100px',
-        marginleft:'500px',
-        padding:'10px'
+
+class GokuTransform extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transformation: "Kaioken",
+      clickCount: 0 
     };
-    const fstyle={
-        backgroundColor:"lightblue",
-        height:'400px',
-        width:'500px'
-    };
-    const handleSubmit = (event) => {
-        event.preventDefault(); 
-        
-        const inputel1 = document.getElementById('in1');
-        const inputval1 = inputel1.value;
-        const inputel2 = document.getElementById('in2');
-        const inputval2 = inputel2.value;
-        const inputel3 = document.getElementById('in3');
-        const inputval3 = inputel3.value;
-    
-        console.log('Name:', inputval1+" "+'Email: ',inputval2+" "+'Message: '+inputval3); 
-      };
-    
-      return (
-        <center><div>
-          <h1>Form</h1>
-          <form onSubmit={handleSubmit} style={fstyle}>
-            <br></br>
-            <br></br>
-            <div>
-              <label>Name:</label>
-              <input
-                type="text"
-                id="in1" style={instyle}
-                required
-              />
-              <br></br>
-              <br></br>
-              <label>Email:</label>
-              <input type='email' id='in2' required style={instyle}></input>
-              <br></br>
-              <br></br>
-              <label>Message:</label><br></br>
-              <textarea placeholder="Enter the Message" rows={5} cols={20} id='in3' required style={instyle}></textarea>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
-        </div></center>
-      );
-};
-export default Day2pah1;
+  }
+  handleButtonClick = () => {
+    this.setState((prevState) => {
+      const newClickCount = prevState.clickCount + 1;
+      
+      if (newClickCount === 4) {
+        return {
+          transformation: prevState.transformation === "Kaioken" ? "SuperSaiyan" : "Kaioken",
+          clickCount: 0
+        };
+      } else {
+        return { clickCount: newClickCount };
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Current Transformation: {this.state.transformation}</h1>
+        <button onClick={this.handleButtonClick}>Click Me</button>
+        {/* <p>Click Count: {this.state.clickCount}</p> */}
+      </div>
+    );
+  }
+}
+
+export default GokuTransform;
