739e6df05395f6a05d4bf9ee0571d8855fd145d3
Yaswanth G <727723eucs282@skcet.ac.in>
2024-10-28 15:32:16 +0530

    DAY5

--- PAH.jsx
+++ PAH.jsx
@@ -1,37 +1,15 @@
-import React from 'react';
-
-class GokuTransform extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      transformation: "Kaioken",
-      clickCount: 0 
-    };
-  }
-  handleButtonClick = () => {
-    this.setState((prevState) => {
-      const newClickCount = prevState.clickCount + 1;
-      
-      if (newClickCount === 4) {
-        return {
-          transformation: prevState.transformation === "Kaioken" ? "SuperSaiyan" : "Kaioken",
-          clickCount: 0
-        };
-      } else {
-        return { clickCount: newClickCount };
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Current Transformation: {this.state.transformation}</h1>
-        <button onClick={this.handleButtonClick}>Click Me</button>
-        {/* <p>Click Count: {this.state.clickCount}</p> */}
-      </div>
-    );
-  }
+import React from "react";
+const Productlist=({ProductsList})=>{
+    return(
+        <ul>
+            {ProductsList.map((prod)=>(
+                <li Key={prod.id}>
+                    <div>{prod.name}</div>
+                    <img src={'/${product.image1}'} alt={prod.name} style={{width:'100px',height:'auto'}}/>
+                    <div>${prod.price.toFixed(2)}</div>
+                </li>
+            ))}
+        </ul>
+    )
 }
-
-export default GokuTransform;
+export default Productlist;
